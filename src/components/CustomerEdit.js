187a12b481d8a9a58e49c539473431af3c370093
Sacks-Lucas <sackslm0@gmail.com>
2021-06-08 16:57:30 -0300

    Seccion 38. Video 502. High Order Component (HOC)

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { reduxForm, Field } from 'redux-form'
-import { connect } from 'react-redux'
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial'
 
 const CustomerEdit = ({name,age,dni}) => {
     return (
@@ -31,13 +31,8 @@ CustomerEdit.propTypes = {
     age:PropTypes.number,
 }
 
-const mapStateToProps= (state, props) =>({
-    initialValues:props
-})
-const mapDispatchToProps= () => ({})
 
 const CustomerEditForm = reduxForm({form: 'CustomerEdit'})(CustomerEdit)
 
-export default connect(mapStateToProps,mapDispatchToProps) (CustomerEditForm)
+export default setPropsAsInitial(CustomerEditForm)
 
-// export default connect(mapStateToProps,mapDispatchToProps) (reduxForm({form: 'CustomerEdit'})(CustomerEdit))
